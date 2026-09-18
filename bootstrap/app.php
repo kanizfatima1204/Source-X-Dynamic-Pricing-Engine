4100e11ba4e00dbfe0bf1d9e56f69f45dfae7863
Kaniz Fatima <kfatima1204@gmail.com>
2026-09-18 22:07:21 +0600

    Fix mixed content blocking by trusting proxies, forcing HTTPS, and adding upgrade-insecure-requests CSP

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,28 @@
 <?php
+
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\URL;
+
 return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(web: __DIR__.'/../routes/web.php', api: __DIR__.'/../routes/api.php', commands: __DIR__.'/../routes/console.php', health: '/up')
-    ->withMiddleware(function (Middleware $middleware) {})
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+    })
     ->withExceptions(function (Exceptions $exceptions) {})
+    ->booting(function () {
+        if (
+            (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https')
+            || (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
+            || env('APP_ENV') === 'production'
+        ) {
+            URL::forceScheme('https');
+        }
+    })
     ->create();
